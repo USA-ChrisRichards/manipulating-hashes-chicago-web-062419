3d3201ac31e8f8be7b31bf4fe1f9c6d5ca692378
Christopher Richards <usa.chrisrichards@gmail.com>
2019-06-20 21:15:05 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -14,7 +14,7 @@ def first_challenge
   }
 
   #your code here
-contacts.each do |person, data|
+contacts["Freddy Mercury"][:favorite_icecream_flavors]
    data.each do |attribute, value|
          if attribute == :favorite_icecream_flavors
       value.each do |flavor|
